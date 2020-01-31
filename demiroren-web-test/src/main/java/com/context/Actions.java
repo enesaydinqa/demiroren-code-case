5a9b2b125fb910000385e0898bdff7ff950f8d91
Enes Aydın <enes.aydin@sahibinden.com>
2020-01-31 22:01:15 +0300

    phase 3

--- demiroren-web-test/src/main/java/com/context/Actions.java
+++ demiroren-web-test/src/main/java/com/context/Actions.java
@@ -12,17 +12,7 @@ public interface Actions
 
     void mouseOver(WebElement element);
 
-    void selectOptionIndex(WebElement element, int index);
-
-    void selectOptionValue(WebElement element, String itemValue);
-
-    void selectOptionVisibleText(WebElement element, String visibleText);
-
     void sendKeys(WebElement element, CharSequence text);
-
-    boolean isDisplayed(WebElement element);
-
-    boolean isAttributePresent(WebElement element, String attribute);
 
     void waitElementToBeClickable(WebElement element);
 
@@ -30,43 +20,7 @@ public interface Actions
 
     void waitElementNotVisible(WebElement element);
 
-    void clearInput(WebElement element);
-
-    void clearMultipleSelectedOption(WebElement element);
-
     String getText(WebElement element);
-
-    String getSelectedOptionText(WebElement element);
-
-    String getAttribute(WebElement element, String attributeName);
-
-    String selectedOptionGetText(WebElement element);
-
-    String selectedOptionGetValue(WebElement element);
-
-    void wait(int seconds) throws InterruptedException;
-
-    void pageLoad();
-
-    void implicitlyWait();
-
-    void assertEquals(Object actual, Object expected);
-
-    void checkBoxChecked(WebElement element);
-
-    void pageRefresh();
-
-    void keysENTER(WebElement element);
-
-    void switchWindowTab(int tab);
-
-    void switchParentFrame();
-
-    void switchFrame(WebElement element);
-
-    String getWindowHandle();
-
-    void deleteCookie();
 
     void scrollToElement(WebElement element);
 
